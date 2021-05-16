bf3d45bfad2c035ba67492e9006a071ee35d6ae0
Khi Hady Sucahyo <khihady.ks@gmail.com>
2021-05-16 22:49:22 +0800

    server streaming rpc

--- greet/greet_client/client.go
+++ greet/greet_client/client.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"fmt"
+	"io"
 	"log"
 
 	"github.com/khihadysucahyo/grpc-go-course/greet/greetpb"
@@ -22,7 +23,8 @@ func main() {
 	c := greetpb.NewGreetServiceClient(cc)
 	// fmt.Printf("Created client: %v", c)
 
-	doUnary(c)
+	// doUnary(c)
+	doServerStreaming(c)
 
 }
 
@@ -43,4 +45,35 @@ func doUnary(c greetpb.GreetServiceClient) {
 	}
 
 	log.Printf("Response from Greet: %v", res.Result)
+}
+
+func doServerStreaming(c greetpb.GreetServiceClient) {
+	fmt.Println("Starting to do server streaming RPC...")
+
+	in := &greetpb.GreetManyTimesRequest{
+		Greeting: &greetpb.Greeting{
+			FirstName: "Khihady",
+			LastName:  "Sucahyo",
+		},
+	}
+
+	inStream, err := c.GreetManyTimes(context.Background(), in)
+
+	if err != nil {
+		log.Fatalf("Error while calling Greet RPC with %v", err)
+	}
+
+	for {
+		msg, err := inStream.Recv()
+		if err == io.EOF {
+			break
+		}
+
+		if err != nil {
+			log.Fatalf("Error while reading stream with	", err)
+		}
+
+		log.Printf("Response from GreatManyTimes: %v", msg.GetResult())
+	}
+
 }
